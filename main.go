b6e8c3447b9826559f834d324763acc8f444edf4
Chris Hubbard <chris@thehubbards.org>
2023-09-08 10:32:00 -0400

    Add intentional error to make sure lint works

--- main.go
+++ main.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+type Test struct {
+	Foo string `json:"foo"`
+	bar string `json:"bar"`
+}
+
 func main() {
+	t := Test{"baz", "quux"}
+	fmt.Println(t)
 	fmt.Println("Hello Aeneas")
 }
