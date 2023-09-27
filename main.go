e297e32720ffe2abcb411716f87201381d2a9620
Chris Hubbard <chris@thehubbards.org>
2023-09-27 12:06:33 -0400

    Add help flag; improve verbose flag

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 var (
 	log         *int
+	help        *bool
 	showVersion bool
 	version     = "dev"
 	commit      = "none"
@@ -16,17 +17,26 @@ var (
 )
 
 func main() {
+	// Parse flags
+	// see: https://pkg.go.dev/github.com/spf13/pflag
 	log = flag.IntP("verbose", "v", 0, "verbose level")
+	flag.Lookup("verbose").NoOptDefVal = "1"
 	flag.BoolVar(&showVersion, "version", false, "display version")
+	help = flag.BoolP("help", "h", false, "display help")
 	flag.Parse()
 
 	if *log > 0 {
 		fmt.Fprintln(os.Stderr, "Logging level:", *log)
 	}
 
+	if *help {
+		flag.Usage()
+		return
+	}
+
 	if showVersion {
 		// GoReleaser automatically sets the version, commit and date
-		// see https://goreleaser.com/cookbooks/using-main.version/
+		// see: https://goreleaser.com/cookbooks/using-main.version/
 		fmt.Printf("go-aeneas version %s (commit %s, built at %s)\n", version, commit, date)
 		return
 	}
