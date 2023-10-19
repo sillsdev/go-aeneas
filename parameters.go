b7b63831d643a84cc831d583182a91a678701d9e
Jonah <thgoldenshank@gmail.com>
2023-10-19 11:53:07 -0400

    Co-authored-by: Ben Steelman <bsteelman@users.noreply.github.com> Co-authored-by: Rioux, Andrew <arioux@liberty.edu> Co-authored-by: John Heid <skved@users.noreply.github.com> Co-authored-by: mthancock <mthancock@users.noreply.github.com>

---
+++ parameters.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"strings"
+)
+
+// https://stackoverflow.com/a/40380147
+// Created a struct to embed map and include into context
+type Parameters struct {
+	m map[string]string
+}
+
+func (p Parameters) Get(key string) string {
+	return p.m[key]
+}
+
+func parseParameters(parameterString string) *Parameters {
+	subParameters := strings.Split(parameterString, "|")
+	mapParameters := make(map[string]string)
+
+	for _, paramValue := range subParameters {
+		kvParams := strings.Split(paramValue, "=")
+		mapParameters[kvParams[0]] = kvParams[1]
+	}
+
+	parameters := &Parameters{mapParameters}
+
+	return parameters
+}
